7ef697f0cddb5ce9403d5267f8897f03c1c7cef6
Tom Wilhelm <hex_machina@live.com>
2026-06-16 22:01:51 -0700

    invert bool value

--- Runtime/ReferenceBool.cs
+++ Runtime/ReferenceBool.cs
@@ -6,6 +6,8 @@ namespace TW.ReferenceObjects
 	[CreateAssetMenu(menuName = "Data/Reference/Bool"), GeneratePropertyBag]
 	public partial class ReferenceBool : ReferenceBase<bool>
 	{
+		[CreateProperty]
+		public bool InvertValue => !Value;
 		public void Toggle()
 		{
 			Value = !Value;
